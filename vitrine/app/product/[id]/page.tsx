199d2aa2e9e5498a1a3fd3877c0633ea0df9227e
Arthur Mondon <arthur-mdn@users.noreply.github.com>
2025-10-30 22:28:26 +0100

    Adjust product page details

--- vitrine/app/product/[id]/page.tsx
+++ vitrine/app/product/[id]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import BuyButton from "@/components/BuyButton";
+import feather from 'feather-icons';
+import { Download } from 'react-feather';
 
 type Product = {
     id: string;
@@ -32,24 +34,53 @@ export default async function ProductPage({ params }: Props) {
         console.log(product)
 
         return (
-            <main className={"product-page"}>
-                <div className="product-image">
-                    <img src={`${process.env.NEXT_PUBLIC_API_URL}/images/${product._id}.png`} />
+            <main className={"product-page fc g2"}>
+                <div className="fr g2">
+                    <div className="product-image">
+                        <img src={`${process.env.NEXT_PUBLIC_API_URL}/images/${product._id}.png`} />
+                    </div>
+
+                    <div className={"fc g0-5"}>
+                        <h1>{product.name}</h1>
+                        <p>{product.metadata.shortDescription.fr}</p>
+                        <p className={"c-t-sec"}>{product.metadata.version}</p>
+                        <button style={{width:'fit-content'}}>
+                            <Download size={16} />
+                            Télécharger l'extension
+                        </button>
+                    </div>
                 </div>
 
-                <h1>{product.name}</h1>
-                <p>{product.metadata.description}</p>
-                <p>Prix</p>
-                <ul>
-                    {product.prices && product.prices.length > 0 ? product.prices.map((price: any) => (
-                        <li key={price._id}>
-                            {price.unitAmount ? `${price.unitAmount} ${price.currency.toUpperCase()}` : 'Gratuit'}
-                            {price.recurring ? ` (récurrent: ${price.recurring.interval})` : ''}
-                            <BuyButton productId={product._id} priceId={price._id} />
-                        </li>
-                    )) : <li>Aucun prix disponible</li>
-                    }
-                </ul>
+                <hr/>
+
+                <div className={"fc g0-5"}>
+                    <h2>Description</h2>
+                    <p>{product.metadata.description.fr}</p>
+                </div>
+
+                {
+                    product.prices && product.prices.length > 0 ? (
+                        <div className={"fc g0-5"}>
+                            <h2>Tarifs</h2>
+                            <div className={"fr g0-5 fw-w"}>
+                                {product.prices.map((price: any) => (
+                                    <div key={price._id} className={"fr g2 box ai-c"}>
+                                        <div>
+                                            {price.metadata?.name && (
+                                                <h3>{price.metadata.name}</h3>
+                                            )}
+                                            <p>{price.unitAmount} {price.currency.toUpperCase()}</p>
+                                            {price.recurring ? ` (récurrent: ${price.recurring.interval})` : ''}
+                                        </div>
+                                        <BuyButton productId={product._id} priceId={price._id} />
+                                    </div>
+                                ))
+                                }
+                            </div>
+                        </div>
+                    ) :
+                        <div>Aucun tarif disponible</div>
+                }
             </main>
         );
     } catch (err: any) {
